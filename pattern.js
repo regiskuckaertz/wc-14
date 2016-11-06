3e9d9fd877c8c1ca929a23836bd7be022f121fa7
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2016-11-06 11:56:29 +0000

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -1,7 +1,7 @@
 /*
  * All the production rules do is replace each character with its expansion.
  * In other words, a global wildcard character regex replacement.
- * This is not a very efficient implementation thought. Given the recursive
+ * This is not a very efficient implementation though. Given the recursive
  * nature of an L-system, dynamic programming by means of memoization and
  * lazy evaluation will yield much faster implementations.
  */
